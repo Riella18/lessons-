ca6e55358afeaaad9f2f59a7a00b44415ff39fc3
Mirela Neziri <mirelaneziri17@gmail.com>
2024-07-17 11:53:22 +0200

    2 classes

--- combined.php
+++ combined.php
@@ -1,6 +1,9 @@
 <?php
 
-$arr = [1,2, 3, 4];
+require 'loops.php';
+<?php
+
+$arr = [1,2,3];
 
 foreach($arr as $a) {
     echo $a ."\n";
--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
 
 require 'loops.php';
+
+class car{
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+        $this->color = $color;
+        $this->model = $model;
+
+    }
+}
+$mycar = new car("blue", "mclaren");
+foreach($mycar as $x => $y) {
+    echo "$x: $y"  . "\n";
+}
+
+class school{
+    public $subject;
+    public $grade;
+    public function __construct($subject, $grade){
+        $this->subject = $subject;
+        $this->grade = $grade;
+    }
+    
+}
+$finals = new school("math", 10);
+foreach($finals as $x => $y){
+    echo "$x: $y" . "\n";
+}
